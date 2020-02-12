5e2815566eac52970c9c06f58eb10c61e47793af
kurakura0147 <45900035+kurakura0147@users.noreply.github.com>
2020-02-12 23:22:23 +0900

    create

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
 const mix = require('laravel-mix')
 
-mix.browserSync('vuesplash.test')
-  .js('resources/js/app.js', 'public/js')
-  .version()
+mix.browserSync({
+  proxy: '0.0.0.0:8081', // アプリの起動アドレス
+  open: false // ブラウザを自動で開かない
+})
+.js('resources/js/app.js', 'public/js')
+.version()
